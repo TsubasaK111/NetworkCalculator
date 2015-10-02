21f87a1fed0ec90332603c8591caebbfd28b5d76
TsubasaKondo <Tsubasa@FujiProvingGrounds.com>
2015-10-02 20:52:24 +0900

    some minor refactoring in delay-calculator-tab

--- endToEndDelay_sync.js
+++ endToEndDelay_sync.js
@@ -1,11 +1,11 @@
 
-var packetInBits = window.prompt("packet size? (kilobits)") * Math.pow(10,3);
-var dataRateInBits = window.prompt("data rate of the link (megabits/sec)?") * Math.pow(10,6);
-var packetizationDelay = (packetInBits/datarateInBits);
+var packetInBits = window.prompt("packet size? (bits)") * Math.pow(10,3);
+var dataRateInBitsPerSec = window.prompt("data rate of the link (megabits/sec)?") * Math.pow(10,6);
+var packetizationDelay = (packetInBits/dataRateInBitsPerSec);
 
 var lengthInMeters = window.prompt("length of the link? (meters)");
-var propagationSpeedInMetersPerSeconds = window.prompt("propagation speed? (m/s)", 2 * Math.pow(10,8) );
-var propagationDelay = lengthInMeters/propagationSpeedInMetersPerSeconds;
+var propagationSpeedInMetersPerSecond = window.prompt("propagation speed? (m/s)", 2 * Math.pow(10,8) );
+var propagationDelay = lengthInMeters/propagationSpeedInMetersPerSecond;
 
 var queueingDelay = window.prompt("queuing delay? (secs)",0);
 var repeatTimes = window.prompt("Does this link repeat? If so, how many times?",1);
